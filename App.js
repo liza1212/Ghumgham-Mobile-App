cd3c1664f2b1399bd48ee1977abbbd26aa2180b0
Reeya <reeya.baidya@gmail.com>
2023-01-25 14:56:07 +0545

    register

--- App.js
+++ App.js
@@ -4,15 +4,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/Home';
 import Login from './components/Login';
+import Register from './components/Register';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOption={{headerShown:false}} initialRouteName='Home'>
+      <Stack.Navigator screenOption={{headerShown:false}} initialRouteName='Register'>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Register" component={Register}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
