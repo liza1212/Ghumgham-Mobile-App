16a158c1f1885ce64c62ebefdec421eff0215532
Liza Shrestha <lisashr12@gmail.com>
2023-01-25 15:39:00 +0545

    Local changes

--- App.js
+++ App.js
@@ -4,14 +4,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/Home';
 import Login from './screens/Login';
-import Register from './components/Register';
+import Register from './screens/Register';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOption={{headerShown:false}} initialRouteName='Register'>
+      <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName='Register'>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Login" component={Login}/>
         <Stack.Screen name="Register" component={Register}/>
